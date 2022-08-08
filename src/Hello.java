abbb70c27554adbc66a1cbbfd1881f57407f1030
SakshamKhanuja <ssk@outlook.ie>
2022-08-08 09:51:50 +0530

    Add comment

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 
+    // Entry point of any Java code.
     public static void main(String[] args) {
         // Prints hello world
         System.out.println("Hello, World!");
